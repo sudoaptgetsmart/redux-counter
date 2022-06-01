69a31dac5aade17cd6ffd4d1ff2886a653efa8c1
sudoaptgetsmart <tfrye463@gmail.com>
2022-06-01 17:16:16 -0500

    Added Calculator function to App.js Init Calculator.js Init calculator.js (might delete) Init Calculator.test.js Added ON_ADD, ON_SUB, ON_MUL, ON_DIV to counter.js

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,7 +1,14 @@
 export const ADD_COUNT = 'counter/ADD_COUNT';
+export const ON_ADD = 'counter/ON_ADD';
+export const ON_SUB = 'counter/ON_SUB';
+export const ON_MUL = 'counter/ON_MUL';
+export const ON_DIV = 'counter/ON_DIV'
 
 const initState = {
-    count: 0
+    count: 0,
+    input1: null,
+    input2: null,
+    total: ''
 }
 
 export default function reducer(state = initState, action) {
@@ -11,6 +18,27 @@ export default function reducer(state = initState, action) {
                 ...state,
                 count: state.count + 1
             }
+        case ON_ADD:
+            return {
+                ...state,
+                total: state.input1 + state.input2
+            }
+        case ON_SUB:
+            return {
+                ...state,
+                total: state.input1 - state.input2
+            }
+        case ON_MUL:
+            return {
+                ...state,
+                total: state.input1 * state.input2
+            }
+        case ON_DIV:
+            return {
+                ...state,
+                total: state.input1 / state.input2
+            }
+
         default:
             return {
                 ...state
