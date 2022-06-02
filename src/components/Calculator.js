2a0ec0b7064b26154df5a72549053ddcd854fb19
sudoaptgetsmart <tfrye463@gmail.com>
2022-06-01 21:53:04 -0500

    Added ON_SET_INPUT1 & ON_SET_INPUT2 Added onInput1Change(event) & onInput2Change(event)

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,17 +1,25 @@
 import {useDispatch,useSelector} from "react-redux";
-import {ON_ADD, ON_SUB, ON_MUL, ON_DIV} from "../modules/counter";
+import {ON_ADD, ON_SUB, ON_MUL, ON_DIV, ON_SET_INPUT1, ON_SET_INPUT2} from "../modules/counter";
 
 export function Calculator({_useDispatch = useDispatch, _useSelector = useSelector}) {
     const dispatch = _useDispatch();
     const input1 = _useSelector(state => state.input1)
     const input2 = _useSelector(state => state.input2)
     const total = _useSelector(state => state.total)
+//TODO explain onInput1Change(event) & onInput2Change(event)
+    function onInput1Change(event) {
+        dispatch({type: ON_SET_INPUT1, value: event.target.value})
+    }
+
+    function onInput2Change(event) {
+        dispatch({type: ON_SET_INPUT2, value: event.target.value})
+    }
 
 
-
+//TODO explain onChange={onInput1Change} value={input1}
     return <>
-        <div><input type={'number'} placeholder={'Some numbers'}/></div>
-        <div><input type={'number'} placeholder={'Some more numbers'}/></div>
+        <div><input onChange={onInput1Change} value={input1} type={'number'} placeholder={'Some numbers'}/></div>
+        <div><input onChange={onInput2Change} value={input2} type={'number'} placeholder={'Some more numbers'}/></div>
         <button onClick={() => dispatch({type: ON_ADD})} type={'button'}>+</button>
         <button onClick={() => dispatch({type: ON_SUB})} type={'button'}>-</button>
         <button onClick={() => dispatch({type: ON_MUL})} type={'button'}>*</button>
--- src/modules/counter.js
+++ src/modules/counter.js
@@ -2,12 +2,14 @@ export const ADD_COUNT = 'counter/ADD_COUNT';
 export const ON_ADD = 'counter/ON_ADD';
 export const ON_SUB = 'counter/ON_SUB';
 export const ON_MUL = 'counter/ON_MUL';
-export const ON_DIV = 'counter/ON_DIV'
+export const ON_DIV = 'counter/ON_DIV';
+export const ON_SET_INPUT1 = 'counter/ON_SET_INPUT1';
+export const ON_SET_INPUT2 = 'counter/ON_SET_INPUT2';
 
 const initState = {
     count: 0,
-    input1: null,
-    input2: null,
+    input1: '',
+    input2: '',
     total: ''
 }
 
@@ -18,25 +20,40 @@ export default function reducer(state = initState, action) {
                 ...state,
                 count: state.count + 1
             }
+// The parseFloat() function parses an argument (converting it to a string first if needed)
+// and returns a floating point number.
         case ON_ADD:
             return {
                 ...state,
-                total: state.input1 + state.input2
+                total: parseFloat(state.input1) + parseFloat(state.input2)
             }
         case ON_SUB:
             return {
                 ...state,
-                total: state.input1 - state.input2
+                total: parseFloat(state.input1) - parseFloat(state.input2)
             }
         case ON_MUL:
             return {
                 ...state,
-                total: state.input1 * state.input2
+                total: parseFloat(state.input1) * parseFloat(state.input2)
             }
         case ON_DIV:
             return {
                 ...state,
-                total: state.input1 / state.input2
+                total: parseFloat(state.input1) / parseFloat(state.input2)
+            }
+//TODO explain ON_SET_INPUT1/2
+        case ON_SET_INPUT1:
+            const num1 = Number.parseFloat(action.value)
+            return {
+                ...state,
+                input1: action.value
+            }
+
+        case ON_SET_INPUT2:
+            return {
+                ...state,
+                input2: action.value
             }
 
         default:
