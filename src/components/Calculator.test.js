5a6d518cb956bdc0830e99185795f8e75fb10ddc
sudoaptgetsmart <tfrye463@gmail.com>
2022-06-01 22:42:05 -0500

    Finished the tests for the buttons.

--- src/components/Calculator.test.js
+++ src/components/Calculator.test.js
@@ -1,34 +1,65 @@
 import {screen, render} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import {Calculator} from "./Calculator";
+import {ON_ADD, ON_MUL, ON_SUB, ON_DIV} from "../modules/counter";
 
 test(
-    'should show a button with "+"',
+    'should show a button with "+" that dispatches ON_ADD when clicked',
     () => {
-        const button = screen.getByText("+");
+
+        const dispatch = jest.fn()
+        render(<Calculator _useDispatch={() => dispatch} _useSelector={() => {
+        }}/>)
+
+        const button = screen.getByText('+');
         expect(button.tagName).toBe('BUTTON')
+
+        userEvent.click(button);
+        expect(dispatch).toHaveBeenCalledWith({type: ON_ADD})
     }
 )
 
 test(
-    'should show a button with "-"',
+    'should show a button with "-" and dispatches ON_SUB when clicked',
     () => {
-        const button = screen.getByText("-");
+        const dispatch = jest.fn()
+        render(<Calculator _useDispatch={() => dispatch} _useSelector={() => {
+        }}/>)
+
+        const button = screen.getByRole('button', {name: "-"});
         expect(button.tagName).toBe('BUTTON')
+
+        userEvent.click(button);
+        expect(dispatch).toHaveBeenCalledWith({type: ON_SUB})
     }
 )
 
 test(
-    'should show a button with"*"',
+    'should show a button with "*" and dispatches ON_MUL when clicked',
     () => {
+        const dispatch = jest.fn()
+        render(<Calculator _useDispatch={() => dispatch} _useSelector={() => {
+        }}/>)
+
         const button = screen.getByText("*");
         expect(button.tagName).toBe('BUTTON')
+
+        userEvent.click(button);
+        expect(dispatch).toHaveBeenCalledWith({type: ON_MUL})
     }
 )
 
 test(
-    'should show a button with "/"',
+    'should show a button with "/" and dispatches ON_DIV when clicked',
     () => {
+        const dispatch = jest.fn()
+        render(<Calculator _useDispatch={() => dispatch} _useSelector={() => {
+        }}/>)
+
         const button = screen.getByText("/");
         expect(button.tagName).toBe('BUTTON')
+
+        userEvent.click(button);
+        expect(dispatch).toHaveBeenCalledWith({type: ON_DIV})
     }
 )
